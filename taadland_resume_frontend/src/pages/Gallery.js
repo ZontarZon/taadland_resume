e4f06bea03f7632247788851828639f06478f23a
Thorya <thoryagudrun@gmail.com>
2022-10-20 20:23:18 -0400

    Add compressed art files, add gallery flexbox, add popup & btn

--- taadland_resume_frontend/src/pages/Gallery.js
+++ taadland_resume_frontend/src/pages/Gallery.js
@@ -1,17 +1,91 @@
-import React from "react";
+import React, {useEffect, useRef, useState} from "react";
+import {FaTimes} from "react-icons/fa";
 import BodyContentHeader from "../common/BodyContentHeader";
-import WorkInProgressMsg from "../common/WorkInProgressMsg";
+import "./Gallery.scss";
+
+const ArtPost = ({url, altText, onClick}) => {
+  return (
+    <div className="art_post" onClick={onClick}>
+      <img className="art_img" src={url} alt={altText} />
+    </div>
+  );
+};
+
 const Gallery = () => {
+  let galleryItemsList = require(`./GalleryItems.json`);
+
+  const [largeImgPopupIndex, setLargeImgPopupIndex] = useState(null);
+  const popupRef = useRef(null);
+
+  /**
+   * Alert if clicked on outside of element
+   * @source: https://stackoverflow.com/a/42234988/9457623
+   */
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (popupRef.current && !popupRef.current.contains(event.target)) {
+        setLargeImgPopupIndex(null);
+      }
+    }
+    // Bind the event listener
+    if (largeImgPopupIndex !== null) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [popupRef, largeImgPopupIndex]);
 
   return (
     <div>
+      <div
+        id="large_art_popup_graybox"
+        style={{display: largeImgPopupIndex !== null ? "flex" : "none"}}
+      >
+        <button
+          id="large_art_popup_close_btn"
+          onClick={() => setLargeImgPopupIndex(null)}
+          alt="close popup"
+        >
+          <FaTimes color="white" size={"40px"} />
+        </button>
+
+        <div id="large_art_popup_container" ref={popupRef}>
+          <img
+            id="large_art_img"
+            className="art_img"
+            src={
+              largeImgPopupIndex !== null
+                ? galleryItemsList[largeImgPopupIndex].url
+                : ""
+            }
+            alt={
+              largeImgPopupIndex !== null
+                ? galleryItemsList[largeImgPopupIndex].altText
+                : ""
+            }
+          />
+        </div>
+      </div>
+
       <BodyContentHeader
         headerTitle="Art Gallery"
         headerSubtitle="View the shinies."
         imgSrc="teila_head.png"
         imgAlt="teila's head"
       />
-      <WorkInProgressMsg />
+
+      <div id="art_posts_flex_container">
+        {galleryItemsList.map((value, index) => (
+          <ArtPost
+            key={index}
+            url={value.url}
+            altText={value.alt_text}
+            onClick={() => setLargeImgPopupIndex(index)}
+          />
+        ))}
+      </div>
     </div>
   );
 };
